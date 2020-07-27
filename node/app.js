c37a8a916ce7c29b8e0e9920bc7289a09cb7155d
Sky020 <2397860h@student.gla.ac.uk>
2020-07-27 22:20:56 +0100

    add express and rust impl

--- node/app.js
+++ node/app.js
@@ -1,19 +1,40 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const express = require('express');
+const URL = 'https://www.google.com/search?q=pound+to+rand&rlz=1C1CHBF_en-GBGB822GB822&oq=pund+to+rand&aqs=chrome.1.69i57j0l5.4103j1j4&sourceid=chrome&ie=UTF-8';
+const { get_max, get_min } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const data = require('./rand_time_data.json');
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
-const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
+app.use(express.json());
+
+app.route('/get_max').get((req, res) => {
+  res.json(JSON.parse(get_max(JSON.stringify(data))));
 });
+app.route('/get_min').get((req, res) => {
+  res.json(JSON.parse(get_min(JSON.stringify(data))));
+})
+// app.route('/update').get((req, res) => {
+//     res.json(getRand());
+// })
+app.get('/', (req, res) => {
+  res.send('<h1>Great British Pound to South African Rand API</h1><h2>Available Endpoints:</h2><ul><li>/get_max</li><li>/get_min</li><li>/update</li></ul>');
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, () => {
+  console.log("App listening on port 3000...")
+})
+
+
+// Future impl.
+// function getRand() {
+//     axios(URL).then(html => {
+//         const $ = cheerio.load(html.data);
+//         const rand = $("td > input");
+//         console.log(rand);
+//         return rand;
+//     })
+
+
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,39 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
+#[derive(Serialize, Deserialize)]
+struct Rand {
+    value: f32,
+    date: String,
+}
+
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn get_max(rand_times: &str) -> String {
+    // Loop through all json data, and find max
+    let data: HashMap<String, String> = serde_json::from_str(rand_times).unwrap();
+    let mut record = Rand {value: 0.0, date: "".to_string()};
+
+    for (k, v) in &data {
+        let v_int: f32 = v.parse().unwrap();
+        if &v_int > &record.value {
+            record = Rand { value: v_int, date: k.to_string() };
+        }
+    }
+    return serde_json::to_string(&record).unwrap();
+}
+
+#[wasm_bindgen]
+pub fn get_min(rand_times: &str) -> String {
+    // Loop through all json data, and find min
+    let data: HashMap<String, String> = serde_json::from_str(rand_times).unwrap();
+    let mut record = Rand {value: 999.0, date: "".to_string()};
+
+    for (k, v) in &data {
+        let v_int: f32 = v.parse().unwrap();
+        if &v_int < &record.value {
+            record = Rand { value: v_int, date: k.to_string() };
+        }
+    }
+    return serde_json::to_string(&record).unwrap();
 }
